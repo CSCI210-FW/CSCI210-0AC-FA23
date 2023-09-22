479ae854d1a161028999bc633cfc83287d2ab094
Angela Venable <avenable11@ivytech.edu>
2023-09-21 20:56:44 -0400

    callback functions 9/21/2023

--- 03_27_2023/main.cpp
+++ 03_27_2023/main.cpp
@@ -2,6 +2,8 @@
 #include <sqlite3.h>
 #include <string>
 
+int callback(void *, int, char **, char **);
+
 int main()
 {
     sqlite3 *db;
@@ -18,7 +20,27 @@ int main()
     {
         std::cout << "Database opened successfully." << std::endl;
     }
+    std::string query = "select * from employee";
+    rc = sqlite3_exec(db, query.c_str(), callback, NULL, NULL);
+    if (rc != SQLITE_OK)
+    {
+        std::cout << "There was an error - select callback: " << sqlite3_errmsg(db) << std::endl;
+        std::cout << query << std::endl;
+    }
 
     sqlite3_close(db);
     return 0;
+}
+
+int callback(void *extData, int numCols, char **values, char **colNames)
+{
+    for (int i = 0; i < numCols; i++)
+    {
+        std::cout << colNames[i] << ": ";
+        if (values[i] != NULL)
+            std::cout << values[i];
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+    return SQLITE_OK;
 }
